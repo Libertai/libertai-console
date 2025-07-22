a33da5c73c45cc20bb0282c4b151f69fcce3fecf
Molaryy <molaryyios@gmail.com>
2025-07-22 11:44:41 +0200

    feat: add buffer config

--- vite.config.ts
+++ vite.config.ts
@@ -11,6 +11,14 @@ export default defineConfig({
 	resolve: {
 		alias: {
 			"@": path.resolve(__dirname, "./src"),
+			buffer: 'buffer',
 		},
+	},
+	define: {
+		global: 'globalThis',
+		'process.env': {},
+	},
+	optimizeDeps: {
+		include: ['buffer']
 	},
 });
