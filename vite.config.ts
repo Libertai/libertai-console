0540796f719a92dd9cbae70f2542824d18439fb8
Reza Rahemtola <reza.rahemtola@epitech.eu>
2025-08-29 00:26:17 +0200

    deps: Upgrade dependencies

--- vite.config.ts
+++ vite.config.ts
@@ -2,14 +2,14 @@ import { defineConfig } from "vite";
 import path from "path";
 
 import react from "@vitejs/plugin-react";
-import { TanStackRouterVite } from "@tanstack/router-plugin/vite";
+import { tanstackRouter } from "@tanstack/router-plugin/vite";
 import tailwindcss from "@tailwindcss/vite";
 import { nodePolyfills } from "vite-plugin-node-polyfills";
 
 // https://vite.dev/config/
 export default defineConfig({
 	plugins: [
-		TanStackRouterVite({ target: "react", autoCodeSplitting: true }),
+		tanstackRouter({ target: "react", autoCodeSplitting: true }),
 		tailwindcss(),
 		react(),
 		nodePolyfills({
